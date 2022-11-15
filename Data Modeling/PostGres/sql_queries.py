b2af05545aab58eec4a40c7344cd182f888a8241
KCvW <Cardinalvonwidder77@gmail.com>
2022-11-15 17:09:51 +0100

    Collect Demo Lessons

--- Data Modeling/PostGres/sql_queries.py
+++ Data Modeling/PostGres/sql_queries.py
@@ -1,9 +1,9 @@
 # DROP TABLES
 
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
-user_table_drop = "DROP TABLE IF EXISTS user"
+users_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
-arist_table_drop = "DROP TABLE IF EXISTS artists"
+artists_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
@@ -11,8 +11,8 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplays (
         songplay_id SERIAL PRIMARY KEY,
-        start_time TIMESTAMP REFERENCES time (start_time),
-        user_id INT REFERENCES users (user_id),
+        start_time TIMESTAMP REFERENCES time (start_time) NOT NULL,
+        user_id INT REFERENCES users (user_id) NOT NULL,
         level VARCHAR,
         song_id VARCHAR REFERENCES songs (song_id),
         artist_id VARCHAR REFERENCES artists (artist_id),
@@ -68,30 +68,41 @@ time_table_create = ("""
 
 # INSERT RECORDS
 
-songplay_table_insert = "INSERT INTO songplay
-    (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    cur.execute(insert_query, data)
+songplay_table_insert = ("""
+    INSERT INTO songplay (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
     
-songplay_table_insert = "INSERT INTO user
-    (user_id, first_name, last_name, gender, level)
-    VALUES (%s, %s, %s, %s, %s)"
-    cur.execute(insert_query, data)
+    
+users_table_insert = ("""
+    INSERT INTO user (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (level)
+    DO UPDATE SET level = EXCLUDED.level
+""")
+    
 
-songplay_table_insert = "INSERT INTO song
-    (song_id, title, artist_id, year, duration)
-    VALUES (%s, %s, %s, %s, %s)"
-    cur.execute(insert_query, data)
+songs_table_insert = ("""
+    INSERT INTO song (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+    
 
-songplay_table_insert = "INSERT INTO artist
-    (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
-    VALUES (%s, %s, %s, %s, %s)"
-    cur.execute(insert_query, data)    
+artists_table_insert = ("""
+    INSERT INTO artist (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+       
 
-songplay_table_insert = "INSERT INTO time
-    (start_time, hour, day, week, month, year, weekday)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    cur.execute(insert_query, data)
+time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING
+""")
+    
 
 # FIND SONGS
 
@@ -104,4 +115,4 @@ song_select = ("""
 # QUERY LISTS
 
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+drop_table_queries = [songplay_table_drop, users_table_drop, song_table_drop, artists_table_drop, time_table_drop]
